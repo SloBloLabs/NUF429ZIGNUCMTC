eb7b0227fe5b65a42788ba6bb1f1827d4de66200
Oliver Walczak <olly.walczak@gmx.de>
2023-05-23 22:12:01 +0200

    Audio Class working

--- App/myAppMain.cpp
+++ App/myAppMain.cpp
@@ -1,7 +1,7 @@
-#include "myAppMain.hpp"
+#include "myAppMain.h"
 #include "main.h"
 #include <cstdio>
-//#include "usbd_cdc_if.h"
+#include "usbd_audio_if.h"
 #include "string.h"
 
 void projectMain() {
@@ -11,13 +11,13 @@ void projectMain() {
     //char const *data = "Hello from NUF429ZIGNUCMTC\n";
 
     while(true) {
-        while(!LL_GPIO_IsInputPinSet(USER_BUTTON_GPIO_Port, USER_BUTTON_Pin));
+        /*while(!LL_GPIO_IsInputPinSet(USER_BUTTON_GPIO_Port, USER_BUTTON_Pin));
 
         for(uint8_t i = 0; i < 20; ++i) {
             LL_GPIO_TogglePin(GPIOB, LED_GREEN_Pin|LED_RED_Pin|LED_BLUE_Pin);
             printf("loop %d\n", i);
             //CDC_Transmit_FS((uint8_t*)data, strlen(data));
             LL_mDelay(50);
-        }
+        }*/
     }
 }
--- USB_DEVICE/App/usbd_audio_if.h
+++ USB_DEVICE/App/usbd_audio_if.h
@@ -1,8 +1,9 @@
 /* USER CODE BEGIN Header */
 /**
   ******************************************************************************
-  * @file           : usbd_dfu_if.h
-  * @brief          : Header for usbd_dfu_if.c file.
+  * @file           : usbd_audio_if.h
+  * @version        : v1.0_Cube
+  * @brief          : Header for usbd_audio_if.c file.
   ******************************************************************************
   * @attention
   *
@@ -16,33 +17,33 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
-
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __USBD_DFU_IF_H__
-#define __USBD_DFU_IF_H__
+#ifndef __USBD_AUDIO_IF_H__
+#define __USBD_AUDIO_IF_H__
 
 #ifdef __cplusplus
  extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
-#include "usbd_dfu.h"
+#include "usbd_audio.h"
+#include "swvPrint.h"
 
 /* USER CODE BEGIN INCLUDE */
 
 /* USER CODE END INCLUDE */
 
-/** @addtogroup STM32_USB_DEVICE_LIBRARY
+/** @addtogroup STM32_USB_OTG_DEVICE_LIBRARY
   * @brief For Usb device.
   * @{
   */
 
-/** @defgroup USBD_MEDIA USBD_MEDIA
-  * @brief Header file for the usbd_dfu_if.c file.
+/** @defgroup USBD_AUDIO_IF USBD_AUDIO_IF
+  * @brief Usb audio interface device module.
   * @{
   */
 
-/** @defgroup USBD_MEDIA_Exported_Defines USBD_MEDIA_Exported_Defines
+/** @defgroup USBD_AUDIO_IF_Exported_Defines USBD_AUDIO_IF_Exported_Defines
   * @brief Defines.
   * @{
   */
@@ -55,7 +56,7 @@
   * @}
   */
 
-/** @defgroup USBD_MEDIA_Exported_Types USBD_MEDIA_Exported_Types
+/** @defgroup USBD_AUDIO_IF_Exported_Types USBD_AUDIO_IF_Exported_Types
   * @brief Types.
   * @{
   */
@@ -68,7 +69,7 @@
   * @}
   */
 
-/** @defgroup USBD_MEDIA_Exported_Macros USBD_MEDIA_Exported_Macros
+/** @defgroup USBD_AUDIO_IF_Exported_Macros USBD_AUDIO_IF_Exported_Macros
   * @brief Aliases.
   * @{
   */
@@ -81,13 +82,13 @@
   * @}
   */
 
-/** @defgroup USBD_MEDIA_Exported_Variables USBD_MEDIA_Exported_Variables
+/** @defgroup USBD_AUDIO_IF_Exported_Variables USBD_AUDIO_IF_Exported_Variables
   * @brief Public variables.
   * @{
   */
 
-/** MEDIA Interface callback. */
-extern USBD_DFU_MediaTypeDef USBD_DFU_fops_FS;
+/** AUDIO_IF Interface callback. */
+extern USBD_AUDIO_ItfTypeDef USBD_AUDIO_fops_FS;
 
 /* USER CODE BEGIN EXPORTED_VARIABLES */
 
@@ -97,10 +98,22 @@ extern USBD_DFU_MediaTypeDef USBD_DFU_fops_FS;
   * @}
   */
 
-/** @defgroup USBD_MEDIA_Exported_FunctionsPrototype USBD_MEDIA_Exported_FunctionsPrototype
+/** @defgroup USBD_AUDIO_IF_Exported_FunctionsPrototype USBD_AUDIO_IF_Exported_FunctionsPrototype
   * @brief Public functions declaration.
   * @{
   */
+
+/**
+  * @brief  Manages the DMA full transfer complete event.
+  * @retval None
+  */
+void TransferComplete_CallBack_FS(void);
+
+/**
+  * @brief  Manages the DMA half transfer complete event.
+  * @retval None
+  */
+void HalfTransfer_CallBack_FS(void);
 
 /* USER CODE BEGIN EXPORTED_FUNCTIONS */
 
@@ -122,5 +135,4 @@ extern USBD_DFU_MediaTypeDef USBD_DFU_fops_FS;
 }
 #endif
 
-#endif /* __USBD_DFU_IF_H__ */
-
+#endif /* __USBD_AUDIO_IF_H__ */
